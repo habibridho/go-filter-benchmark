0d03b796308c70427aced9890b5da928c044a9e7
Habib Ridho <habibridho@live.com>
2019-12-13 18:03:57 +0700

    Add 100 kb test

--- filter/filter_test.go
+++ filter/filter_test.go
@@ -1,6 +1,12 @@
 package filter
 
-import "testing"
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"testing"
+)
 
 func BenchmarkFilter(b *testing.B) {
 	for i := 0; i < b.N; i++ {
@@ -18,4 +24,35 @@ func BenchmarkFilterString(b *testing.B) {
 			return val != "c"
 		})
 	}
+}
+
+func BenchmarkFilterWith100kbText(b *testing.B) {
+	arr := constructLipsumArr()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Filter(arr, func(val interface{}) bool {
+			return val != "c"
+		})
+	}
+}
+
+func BenchmarkFilterStringWith100kbText(b *testing.B) {
+	arr := constructLipsumArr()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		FilterString(arr, func(val string) bool {
+			return val != "c"
+		})
+	}
+}
+
+func constructLipsumArr() []string {
+	file, err := os.Open("lipsum.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	byteArr, _ := ioutil.ReadAll(file)
+	arr := strings.Split(string(byteArr), "")
+	return arr
 }
